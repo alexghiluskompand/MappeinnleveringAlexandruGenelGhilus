1e58f77781ea503ddbfb08cf8b6c99de3c42150d
alexandrughilus <34506568+alexandrughilus@users.noreply.github.com>
2019-08-06 21:53:22 +0200

    Added functionallity for adding tasks into the Kanban boardo

--- Innlevering1-AlexandruGenelGhilus/scripts/oppdrag1-kanbanboard.js
+++ Innlevering1-AlexandruGenelGhilus/scripts/oppdrag1-kanbanboard.js
@@ -1,8 +1,54 @@
-$(function() {
+$(function () {
+    /// Draggable tab-elements
+
     $(".tab-content").sortable({
         revert: true
     });
-    $(".tab-element").draggable({connectToSortable: ".tab-content"});
+    $(".tab-element").draggable({
+        connectToSortable: ".tab-content",
+        scroll: "false"
+    });
     $(".tab-content").droppable();
-            
+
+    ///Add tab elements (tasks)
+
+    $("#add-task").click(function () {
+        $("#create-task").html(
+            "<h3>Add task</h3>" +
+            "<textarea id='task-input'></textarea>" +
+            "<button id='confirm-task' class='task-button'>Confirm</button>" +
+            "<button id='cancel-task' class='task-button'>Cancel</button>"
+        ).css({
+            "display": "block",
+            "height": "300px",
+            "margin-bottom": "10px"
+        });
+
+        $("#cancel-task").click(function () {
+            $("#create-task").css("display", "none");
+        });
+
+        $("#confirm-task").click(function () {
+
+            var userInput = $("#task-input").val();
+
+            $("#to-do-content").append(
+                "<div class='tab-element'>" +
+                "<div class='priority'></div>" +
+                "<p>" + userInput + "</p>" +
+                "</div>"
+            )
+            $(".tab-content").sortable({
+                revert: true
+            });
+            $(".tab-element").draggable({
+                connectToSortable: ".tab-content"
+            });
+            $(".tab-content").droppable();
+
+            ;
+
+            $("#create-task").css("display", "none");
+        });
+    });
 });
