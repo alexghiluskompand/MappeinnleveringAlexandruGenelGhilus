d1e7c8a15a2f62284d96c931ba621bdc83083180
alexandrughilus <34506568+alexandrughilus@users.noreply.github.com>
2019-08-06 23:15:34 +0200

    Started with script structure

---
+++ Innlevering1-AlexandruGenelGhilus/scripts/oppdrag1App.js
@@ -0,0 +1,34 @@
+$(function () {
+    //HTML-objects
+
+    let addTask, createTask, cancelTask, confirmTask, userInput, tabContent, tabElement;
+
+    const init = function () {
+        
+        const setElements = function() {
+            addTask = $("#add-task");
+            createTask = $("#create-task");
+            cancelTask = $("#cancel-task");
+            confirmTask = $("#confirm-task");
+            userInput = $("#task-input");
+            tabContent = $(".tab-content");
+            tabElement = $(".tab-element");
+        }();
+        
+        const setEvents = function() {
+            addTask.click(function() {
+                
+            });
+            
+            
+        }();
+        
+        
+        
+        
+        
+        
+    }();
+});
+
+//To be continued...
