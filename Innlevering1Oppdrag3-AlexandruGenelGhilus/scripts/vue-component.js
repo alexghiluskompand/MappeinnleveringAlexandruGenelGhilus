924338c00f03613c35104ad335daf1df685260fb
alexandrughilus <34506568+alexandrughilus@users.noreply.github.com>
2019-08-08 22:11:57 +0200

    Design changes and added a JSON-list

--- Innlevering1Oppdrag3-AlexandruGenelGhilus/scripts/vue-component.js
+++ Innlevering1Oppdrag3-AlexandruGenelGhilus/scripts/vue-component.js
@@ -5,22 +5,25 @@ Vue.component('product', {
     <h2>{{ title }}</h2>
     <p>{{ platform }}</p>
     <p>In stock: {{ stock }} </p>
-    <button v-on:click="cart++">Add to cart</button>
+    <button v-on:click="stock--">Add to cart</button>
 </div>
 
 `,
     data() {
         return {
+            id: 1,
             image: "images/PS4/uncharted4.jpg",
             stock: 10,
             platform: "Playstation 4",
             title: "Uncharted 4"
         }
     },
-    
+
+    methods: {}
+
 })
 
-var app = new Vue({
-  el: '#product-section',
-  data: {}
+var product = new Vue({
+    el: '#product-section',
+    data: {}
 })
