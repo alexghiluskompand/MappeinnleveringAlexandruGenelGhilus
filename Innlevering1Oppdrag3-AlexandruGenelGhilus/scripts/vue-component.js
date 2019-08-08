d579dc46a8d4db8cd27264c415ea7f6622413ede
alexandrughilus <34506568+alexandrughilus@users.noreply.github.com>
2019-08-08 20:35:03 +0200

    Product component and style added

--- Innlevering1Oppdrag3-AlexandruGenelGhilus/scripts/vue-component.js
+++ Innlevering1Oppdrag3-AlexandruGenelGhilus/scripts/vue-component.js
@@ -1,14 +1,26 @@
 Vue.component('product', {
-    template:``,
+    template: `
+<div class="product">
+    <img :src="image" class="image">
+    <h2>{{ title }}</h2>
+    <p>{{ platform }}</p>
+    <p>In stock: {{ stock }} </p>
+    <button v-on:click="cart++">Add to cart</button>
+</div>
+
+`,
     data() {
         return {
+            image: "images/PS4/uncharted4.jpg",
+            stock: 10,
+            platform: "Playstation 4",
+            title: "Uncharted 4"
         }
     },
-    methods: {
-        
-    },
-    computed: {
-        
-    }
     
+})
+
+var app = new Vue({
+  el: '#product-section',
+  data: {}
 })
